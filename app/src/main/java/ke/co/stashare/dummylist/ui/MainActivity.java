2c6346c1e9941d387ea0e26de42d6f0c712ea8e0
Ken Wainaina <wainainaken@gmail.com>
2017-01-24 20:34:49 +0300

    Initial commit

---
+++ app/src/main/java/ke/co/stashare/dummylist/ui/MainActivity.java
@@ -0,0 +1,20 @@
+package ke.co.stashare.dummylist.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import ke.co.stashare.dummylist.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar1) ;
+
+        setSupportActionBar(toolbar);
+    }
+}
